9906d6b0568232ca7fb9153fcdf7a24f62fdcf5e
Ryan Allen <ryangallen@protonmail.com>
2025-04-23 00:47:05 -0400

    tidying

--- app/src/main/java/com/nayef/simplenote/ui/components/TopAppBar.kt
+++ app/src/main/java/com/nayef/simplenote/ui/components/TopAppBar.kt
@@ -1,17 +1,17 @@
 package com.nayef.simplenote.ui.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.layout.padding
 import com.nayef.simplenote.ui.theme.poppins
 
 @Composable
-fun SimpleNotesTopBar(title: String) {
+fun TopAppBar(title: String) {
     Text(
         text = title,
         style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 25.sp, fontFamily = poppins),
