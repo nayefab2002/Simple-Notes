41a5c41300042dde8f5de320e9c506b71bc4728d
Ryan Allen <ryangallen@protonmail.com>
2025-04-22 23:50:55 -0400

    Add Trash Screen and navigation

---
+++ app/src/main/java/com/nayef/simplenote/ui/screens/TrashScreen.kt
@@ -0,0 +1,56 @@
+package com.nayef.simplenote.ui.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+import com.nayef.simplenote.data.Note
+import com.nayef.simplenote.data.NotesViewModel
+import com.nayef.simplenote.ui.components.BottomNavigationBar
+import com.nayef.simplenote.ui.components.NoteCard
+import com.nayef.simplenote.ui.components.NoteCardMode
+import com.nayef.simplenote.ui.components.SimpleNotesTopBar
+
+
+@Composable
+fun TrashScreen(viewModel: NotesViewModel, navController: NavController) {
+    val notes by viewModel.deletedNotes.collectAsState(initial = emptyList())
+    var showDialog by remember { mutableStateOf(false) }
+    var existingNote by remember { mutableStateOf<Note?>(null) }
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = {
+            showDialog = true
+            existingNote = null
+
+        }) {
+            Icon(Icons.Default.Add, contentDescription = "Add Note")
+
+        }
+    }, topBar = {
+        SimpleNotesTopBar("Trash")
+    }, bottomBar = {BottomNavigationBar(navController,true)}) { padding ->
+        LazyColumn(modifier = Modifier.padding(padding)) {
+            items(notes.size) { index ->
+                NoteCard(
+                    note = notes[index],
+                    onDelete = {viewModel.permanentlyDeleteNote(notes[index])},
+                    onRestore = {viewModel.restoreNote(notes[index])},
+                    mode = NoteCardMode.Trash
+                )
+            }
+
+        }
+    }
+}
